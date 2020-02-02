4b8c0cfc67eb2ab609ead579c19c9c678286d790
Daniel Q Parra <queraltoies@gmail.com>
2020-02-02 17:44:28 +0100

    updates

---
+++ src/components/Cuerpo.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="cuerpo">
+    <h1>cuerpo Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'cuerpo',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .cuerpo {
+
+  }
+</style>
---
+++ src/components/Lista.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+
+  <section class="lista">
+    <h1>lista Component</h1>
+    <section>
+        <input type="text" size="74" v-model="texNota" v-on:keyup.enter="addRecord">      
+        <button class="btn btn-success" v-on:click="addRecord">+</button>
+        {{salto}}
+        <label class="">Filtro:</label>
+        <input type="text" size="71" v-model="rating" v-on:keyup.enter="ordenar">     
+    </section>
+  </section>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'lista',
+    props: ["text"],
+
+    mounted () {
+
+    },
+
+    data () {
+      return {
+
+      }
+    },
+
+    methods: {
+
+    },
+
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .lista {
+
+  }
+</style>
