37762f921b8f5d592ef5a923210e041f69eb1d1f
Daniel Q Parra <queraltoies@gmail.com>
2020-02-02 18:09:02 +0100

    update

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],  
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/public/index.html'
-  : '/'
+  ? '/public/'
+  : '/index.html'
 }
 
